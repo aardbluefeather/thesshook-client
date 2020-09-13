86541d21b822b462c33f87478bcd4662a0d7a9f1
aardbluefeather <bluefeatherwashere@gmail.com>
2020-09-13 22:02:41 +0200

    reset colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,21 +5,30 @@ import '@/sass/overrides.sass';
 
 Vue.use(Vuetify);
 
-const theme = {
-  primary: '#E91E63',
-  secondary: '#9C27b0',
-  accent: '#9C27b0',
-  info: '#00CAE3'
-};
-
 export default new Vuetify({
   lang: {
     t: (key, ...params) => i18n.t(key, params)
   },
   theme: {
     themes: {
-      dark: theme,
-      light: theme
+      light: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
+      dark: {
+        primary: '#2196F3',
+        secondary: '#424242',
+        accent: '#FF4081',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
     }
   }
 });
